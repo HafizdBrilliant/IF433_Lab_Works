b2ba7ea04862216d39f128c104c76937398eee15
Brilliant Hafizd <brillhzd@gmail.com>
2026-02-03 23:58:54 +0700

    week01: add calculateStatus function

--- src/oop_00000130613_BrilliantHafizdHermawan/week01/GradingSystem.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week01/GradingSystem.kt
@@ -1,15 +1,23 @@
 package oop_00000130613_BrilliantHafizdHermawan.week01
 
-fun main () {
+// BAGIAN 1: Tulis DI LUAR main()
+// Fungsi ini tugasnya mengecek apakah lulus atau tidak
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
+fun main() {
+    // Kita set nilai score (atau bisa dari input user)
     val score = 85
-val grade = when (score) {
-    in 90..100 -> "A"
-    in 80..89 -> "B"
-    in 70..79 -> "C"
-    else -> "D"
-}
 
+    // Kode Grade (A/B/C/D) yang tadi
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
     println("Grade Kamu: $grade")
+
+    // BAGIAN 2: Panggil DI DALAM main()
+    // Ini akan mencetak "Status: Lulus" atau "Status: Tidak Lulus"
+    println("Status: ${calculateStatus(score)}")
 }
-
-
