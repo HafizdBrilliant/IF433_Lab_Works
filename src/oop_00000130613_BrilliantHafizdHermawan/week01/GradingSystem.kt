5fb0f7891acb610827ca7a263bf0e16f186c6aa2
Brilliant Hafizd <brillhzd@gmail.com>
2026-02-04 05:51:38 +0700

    weekk01: add calculateStatus function

--- src/oop_00000130613_BrilliantHafizdHermawan/week01/GradingSystem.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week01/GradingSystem.kt
@@ -1,13 +1,8 @@
 package oop_00000130613_BrilliantHafizdHermawan.week01
 
-// [DARI GAMBAR 1] Tulis DI LUAR main()
-// Fungsi expression body untuk mengecek status lulus/tidak
 fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
-
 fun main() {
-    // 1. Bagian Score & Grade (Kode Sebelumnya)
     val score = 85
-
     val grade = when (score) {
         in 90..100 -> "A"
         in 80..89 -> "B"
@@ -15,15 +10,5 @@ fun main() {
         else -> "D"
     }
     println("Grade Kamu: $grade")
-
-    // [DARI GAMBAR 1] Panggil DI DALAM main()
     println("Status: ${calculateStatus(score)}")
-
-    // [DARI GAMBAR 2] Tambahkan di akhir main() - Null Safety
-    val studentId: String? = null
-
-    // Jika null, gunakan nilai default 0 (menggunakan Elvis Operator ?:)
-    val idLength = studentId?.length ?: 0
-
-    println("Panjang ID: $idLength")
 }
