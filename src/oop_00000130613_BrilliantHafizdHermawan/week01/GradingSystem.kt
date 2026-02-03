95baca05e87d49b951c65aaebe2fdf12fa71dc0a
Brilliant Hafizd <brillhzd@gmail.com>
2026-02-03 23:45:15 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000130613_BrilliantHafizdHermawan/week01/GradingSystem.kt
@@ -0,0 +1,10 @@
+package oop_00000130613_BrilliantHafizdHermawan.week01
+
+fun main () {
+    var name: String = "john Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
+
+
