7425ebc4696f1fa8d2fabb1135ccdfc2f82e34c3
Brilliant Hafizd <brillhzd@gmail.com>
2026-02-12 13:24:08 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000130613_BrilliantHafizdHermawan/week02/Hero.kt
@@ -0,0 +1,25 @@
+package oop_00000130613_BrilliantHafizdHermawan.week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+
+    fun attack(targetName: String){
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+        println("$name menerima damage besar $damage. (Sisa HP: $hp)")
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+
+}
