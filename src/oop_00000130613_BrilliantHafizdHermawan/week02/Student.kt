7ca51283e2c94545fb8f50c8a8d9075a30c7680a
Brilliant Hafizd <brillhzd@gmail.com>
2026-02-12 09:18:49 +0700

    week02: add secondary constructor

--- src/oop_00000130613_BrilliantHafizdHermawan/week02/Student.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week02/Student.kt
@@ -15,4 +15,9 @@ class Student (
         }
     }
 
+    constructor(name: String, nim: String) : this (name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+
+
 }
