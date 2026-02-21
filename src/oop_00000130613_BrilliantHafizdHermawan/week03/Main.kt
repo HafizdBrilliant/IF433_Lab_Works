362dd18fcee2e9ffc3eb362c1ba6733e4893e9c3
Brilliant Hafizd <brillhzd@gmail.com>
2026-02-22 06:23:51 +0700

    week03: add private property

--- src/oop_00000130613_BrilliantHafizdHermawan/week03/Main.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week03/Main.kt
@@ -3,11 +3,15 @@ package oop_00000130613_BrilliantHafizdHermawan.week03
 fun main() {
     val e = Employee("Budi")
 
-    e.salary = -1000
+    // 1. Test Validasi Salary
+    e.salary = -1000 // Harusnya print Error
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
+    // 2. Test Encapsulation
     e.increasePerformance()
+    e.performanceRating = 5 // Coba uncomment, pasti Merah (Error)
 
+    // 3. Test COmputed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
