c4cd2538ff832c77d0600b35842070222741d265
Brilliant Hafizd <brillhzd@gmail.com>
2026-02-22 07:00:57 +0700

    week03: (task 2) create Player class with private xp

--- src/oop_00000130613_BrilliantHafizdHermawan/week03/Player.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week03/Player.kt
@@ -1,4 +1,24 @@
 package oop_00000130613_BrilliantHafizdHermawan.week03
 
-class Player {
+class Player(val username: String) {
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp /100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount > 0) {
+            val levelSebelumnya = this.level
+
+            xp += amount
+
+            if (this.level > levelSebelumnya) {
+                println("Level Up! Selamat $username naik ke level $level")
+            } else {
+                println("$username mendapat $amount XP. Total XP: $xp, Level: $level)")
+            }
+        } else {
+            println("Peringatan: XP yang dita,bahkan harus angka positif!")
+        }
+    }
 }
