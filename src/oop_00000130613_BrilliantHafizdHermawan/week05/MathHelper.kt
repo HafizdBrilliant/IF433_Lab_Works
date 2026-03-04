ec1d4a60da30f0fa062d96e71b8fb1272040e071
Brilliant Hafizd <brillhzd@gmail.com>
2026-03-04 23:24:14 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000130613_BrilliantHafizdHermawan/week05/MathHelper.kt
@@ -0,0 +1,19 @@
+package oop_00000130613_BrilliantHafizdHermawan.week05
+
+class MathHelper {
+
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
