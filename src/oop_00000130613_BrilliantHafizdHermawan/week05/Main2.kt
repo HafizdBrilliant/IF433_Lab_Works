07dd4c0bb88cce77bbd2d967f7238eeff61e386f
Brilliant Hafizd <brillhzd@gmail.com>
2026-03-04 23:35:33 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000130613_BrilliantHafizdHermawan/week05/Main2.kt
@@ -0,0 +1,19 @@
+package oop_00000130613_BrilliantHafizdHermawan.week05
+
+fun main() {
+
+    println("\n=== TUGAS MANDIRI 2: SISTEM PEMBAYARAN ===")
+
+    val dompetHafizd = EWallet("E-Wallet Hafizd", 50000.0)
+    val kartuHafizd = CreditCard("CC Hafizd", 100000.0)
+
+    val daftarPembayaran: List<PaymentMethod> = listOf(dompetHafizd, kartuHafizd)
+
+
+    for (metode in daftarPembayaran) {
+        println("\nMemproses pembayaran 75000.0 untuk akun: ${metode.accountName}")
+
+
+        metode.processPayment(75000.0)
+    }
+}
