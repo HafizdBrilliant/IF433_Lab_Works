1cfb461adb25a446286139de51346246e299a540
Brilliant Hafizd <brillhzd@gmail.com>
2026-03-04 23:37:54 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_00000130613_BrilliantHafizdHermawan/week05/Main2.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week05/Main2.kt
@@ -6,14 +6,16 @@ fun main() {
 
     val dompetHafizd = EWallet("E-Wallet Hafizd", 50000.0)
     val kartuHafizd = CreditCard("CC Hafizd", 100000.0)
-
     val daftarPembayaran: List<PaymentMethod> = listOf(dompetHafizd, kartuHafizd)
-
-
     for (metode in daftarPembayaran) {
         println("\nMemproses pembayaran 75000.0 untuk akun: ${metode.accountName}")
-
-
         metode.processPayment(75000.0)
+        if (metode is EWallet) {
+            println("=> [Sistem Penyelamat] Terdeteksi E-Wallet. Saldo kurang? Mari Top Up otomatis!")
+            metode.topUp(50000.0)
+
+            println("=> Mengulang transaksi pembayaran...")
+            metode.processPayment(75000.0)
+        }
     }
 }
