77efb182b4bb420d1f53a375bec2a23c63442a70
Brilliant Hafizd <brillhzd@gmail.com>
2026-03-04 21:47:04 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000130613_BrilliantHafizdHermawan/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000130613_BrilliantHafizdHermawan.week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki vody (tanpa kerung kurawal)
+    abstract fun bekerja()
+}
