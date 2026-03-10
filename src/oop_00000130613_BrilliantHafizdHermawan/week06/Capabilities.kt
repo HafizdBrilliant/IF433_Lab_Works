fe5d41f6b2785a6c944973e532d5e48e8355af3f
Brilliant Hafizd <brillhzd@gmail.com>
2026-03-10 23:57:03 +0700

    week06: create compound interfaces

---
+++ src/oop_00000130613_BrilliantHafizdHermawan/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000130613_BrilliantHafizdHermawan.week06
+
+// Interface -> Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
