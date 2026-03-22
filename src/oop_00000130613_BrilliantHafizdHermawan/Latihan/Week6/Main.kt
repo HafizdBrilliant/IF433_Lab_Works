6df941d9958111230b4a6a304cfe4a2bbcf3246d
Brilliant Hafizd <brillhzd@gmail.com>
2026-03-22 14:00:52 +0700

    week07: (task) create GameManger singleton

--- src/oop_00000130613_BrilliantHafizdHermawan/Latihan/Week6/Main.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/Latihan/Week6/Main.kt
@@ -1,2 +1,24 @@
 package oop_00000130613_BrilliantHafizdHermawan.Latihan.Week6
 
+class Mahasiswa(val nama: String) {
+
+    // Ini adalah area Singleton yang nempel di KELAS Mahasiswa
+    companion object {
+        const val NAMA_KAMPUS = "Universitas Multimedia Nusantara"
+
+        fun infoKampus() {
+            println("Ini adalah kampus $NAMA_KAMPUS")
+        }
+    }
+}
+
+fun main() {
+    // 1. Membuat objek kelas biasa (bisa banyak)
+    val mhs1 = Mahasiswa("Budi")
+    val mhs2 = Mahasiswa("Siti")
+
+    // 2. Memanggil anggota companion object
+    // DIPANGGIL LEWAT NAMA KELASNYA (Mahasiswa), BUKAN LEWAT OBJEKNYA (mhs1/mhs2)
+    println(Mahasiswa.NAMA_KAMPUS)
+    Mahasiswa.infoKampus()
+}
