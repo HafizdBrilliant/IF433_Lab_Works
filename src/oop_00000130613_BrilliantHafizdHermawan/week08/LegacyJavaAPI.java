16bf94f737336041f59f129766893b40ccb83a66
Brilliant Hafizd <brillhzd@gmail.com>
2026-04-17 20:52:51 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000130613_BrilliantHafizdHermawan/week08/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_00000130613_BrilliantHafizdHermawan.week08;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
