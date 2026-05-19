48f483f59230898d51d782b850d52e884ee04325
Brilliant Hafizd <brillhzd@gmail.com>
2026-05-19 21:20:12 +0700

    week13: impleemnt appendtext operation

--- src/oop_00000130613_BrilliantHafizdHermawan/week13/BasicFileIO.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week13/BasicFileIO.kt
@@ -7,4 +7,7 @@ fun main() {
     val file = File("src/oop_00000130613_BrilliantHafizdHermawan/week13/notes.txt")
     file.writeText("Line 1: Inisialisasi sistem.\n")
     println("File berhasil dibuat dan ditulis")
+
+    file.appendText("Line 2: menambahkan konfigurasi baru.\n")
+    println("Teks berhasil diappend.")
 }
