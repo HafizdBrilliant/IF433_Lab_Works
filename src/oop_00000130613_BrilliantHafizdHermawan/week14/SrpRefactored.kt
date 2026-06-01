660aa534d9b81f4b9fc90df07305560445382d38
Brilliant Hafizd <brillhzd@gmail.com>
2026-06-01 16:40:54 +0700

    week14: extract EmailService to fix SRP

--- src/oop_00000130613_BrilliantHafizdHermawan/week14/SrpRefactored.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week14/SrpRefactored.kt
@@ -1,2 +1,12 @@
 package oop_00000130613_BrilliantHafizdHermawan.week14
 
+class UserValidator {
+    fun validate(user: User): Boolean = user.email.contains("@") && user.age >= 18
+
+    fun save(user: User) {
+        println("Saving user: ${user.name} to Database")
+    }
+}
+
+git add .
+git commit -m "week14: extract EmailService to fix SRP"
