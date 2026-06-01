01f057efee7b504643e1777bcce89ba2455d79d3
Brilliant Hafizd <brillhzd@gmail.com>
2026-06-01 16:53:34 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000130613_BrilliantHafizdHermawan/week14/EcommerceTask.kt
+++ src/oop_00000130613_BrilliantHafizdHermawan/week14/EcommerceTask.kt
@@ -17,3 +17,40 @@ class BadOrderProcessor {
     }
 }
 
+// SRP + DIP
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+class CsvOrderRepository(private val file: File = File("orders_refactored.csv")) : OrderRepository {
+    override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
+        file.appendText("$itemName,$finalPrice,$customerType\n")
+    }
+}
+
+interface NotificationService {
+    fun sendNotification(itemName: String)
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(itemName: String) {
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
+
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.90
+            else -> basePrice
+        }
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        repo.saveOrder(itemName, finalPrice, customerType)
+        notifier.sendNotification(itemName)
+    }
+}
+
